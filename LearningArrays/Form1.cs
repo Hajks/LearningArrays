d47ac05c440386e803d510c113c722cd677741ef
Hajk <tunczorx@gmail.com>
2018-03-21 11:22:41 +0100

    Simple Arrays, checking how it works

--- LearningArrays/Form1.cs
+++ LearningArrays/Form1.cs
@@ -12,9 +12,17 @@ namespace LearningArrays
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1()// Our app will generate random menu for sandwiches
         {
             InitializeComponent();
+            MenuMaker menu = new MenuMaker() { Randomizer = new Random() }; //Randomizer = new Random() is object initializer
+            label1.Text = menu.GetMenuItem();
+            label2.Text = menu.GetMenuItem();
+            label3.Text = menu.GetMenuItem();
+            label4.Text = menu.GetMenuItem();
+            label5.Text = menu.GetMenuItem();
+            label6.Text = menu.GetMenuItem();
+
         }
     }
 }
---
+++ LearningArrays/MenuMaker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LearningArrays
+{
+    public class MenuMaker
+    {
+      
+        public Random Randomizer;
+        string[] Meats = { "Pieczona wołowina", "Salami", "Indyk", "Szynka", "Karkówka" };
+        string[] Condiments = { "żółta musztarda", "brązowa musztarda", "musztarda miodowa", "majonez", "przyprawy", "sos francuski" };
+        string[] Breads = { "chleb ryżowy", "chleb biały", "chleb zbożowy", "pumpernikiel", "chleb włoski", "bułka" };
+
+        public string GetMenuItem()
+        {
+            string randomMeat = Meats[Randomizer.Next(Meats.Length)]; // Checking length of our string, and taking one random component
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+            return randomMeat + ", " + randomCondiment + ", "+ randomBread;
+        }
+    }
+    
+}
